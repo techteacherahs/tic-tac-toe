3c4ce67d321f2d9de9cd3901bb51afcb3ac50ba1
techteacherahs <75147482+techteacherahs@users.noreply.github.com>
2023-01-18 14:28:31 -0500

    Create main.py
    Introductory comments only

---
+++ main.py
@@ -0,0 +1,2 @@
+#Use this file to create your Tic-Tac-Toe game in a Tkinter GUI. Be sure to change your PyCharm environment if needed to ensure functionality - Open Preferences > Project > Python Interpreter > Select Conda 3.8 if available - it may be list another project next to the name, that's ok.
+#Find the instructions for this assignment on Google Classroom
